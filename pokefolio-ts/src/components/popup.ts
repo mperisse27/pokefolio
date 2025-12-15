daac6852c9b896405a314e2b2adfb29212426ef6
Matteo Perisse <matteo.perisse@gmail.com>
2025-12-15 15:22:16 +0100

    fix: cleaned popup and added link text effects

--- pokefolio-ts/src/components/popup.ts
+++ pokefolio-ts/src/components/popup.ts
@@ -1,5 +1,3 @@
-import { Graphics, Container, Application, type Renderer, Text } from "pixi.js";
-
 export class Popup {
   public line1: HTMLElement;
   public line2: HTMLElement;
@@ -17,51 +15,6 @@ export class Popup {
     this.lineCounter = 0;
   }
 
-  buildContainer = (app: Application<Renderer>) => {
-    const container = new Container();
-    const boxWidth = app.screen.width;
-    const boxHeight = 100;
-
-    const textBox = new Graphics()
-      .rect(0, 0, boxWidth, boxHeight)
-      .fill(0xffffff)
-      .stroke({ color: 0x000000, width: 8 });
-    textBox.y = app.screen.height - boxHeight;
-    textBox.x = 0;
-    textBox.zIndex = -1;
-    container.addChild(textBox);
-
-    const line1 = new Text({
-      text: "",
-      style: {
-        fontFamily: "my32bit",
-        fontSize: 32,
-        fill: 0x000000,
-      }
-    });
-    line1.x = 16;
-    line1.y = app.screen.height - boxHeight + 12;
-    container.addChild(line1);
-  
-    const line2 = new Text({
-      text: "",
-      style: {
-        fontFamily: "my32bit",
-        fontSize: 32,
-        fill: 0x000000,
-      }
-    });
-    line2.x = 16;
-    line2.y = app.screen.height - boxHeight + 50;
-    container.addChild(line2);
-
-    container.visible = false;
-    container.interactive = true;
-    container.zIndex = 1000; // Ensure the popup is above other elements
-
-    return { container, line1, line2 };
-  }
-
   public changeText(text: string[], url?: string, details?: string): boolean {
     if (this.lineCounter == 0) {
       this.container.classList.remove("hidden");
@@ -69,6 +22,7 @@ export class Popup {
       this.fullLine2 = text[1] ?? "";
       this.lineCounter = 2;
       if (url) {
+        this.line2.classList.add("underline", "text-blue-500");
         this.container.onclick = () => window.open(url, "_blank")?.focus();
         this.container.classList.add("cursor-pointer");
       }
@@ -87,6 +41,7 @@ export class Popup {
       this.fullLine1 = "";
       this.fullLine2 = "";
       this.lineCounter = 0;
+      this.line2.classList.remove("underline", "text-blue-500");
       this.container.onclick = null;
       this.container.classList.remove("cursor-pointer");
       if (details) {
