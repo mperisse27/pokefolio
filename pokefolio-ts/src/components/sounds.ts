ab322e6d8772055437a135a0d27d858329943b65
Matteo Perisse <matteo.perisse@gmail.com>
2026-01-26 17:43:01 +0100

    fix: made sound effects mute consistent with bgm

--- pokefolio-ts/src/components/sounds.ts
+++ pokefolio-ts/src/components/sounds.ts
@@ -1,9 +1,11 @@
 import { Howl } from 'howler';
 
+let soundEffectVolume = 0.05;
+
 export const bgm = new Howl({
   src: ['/sounds/bgm.mp3'],
   loop: true,
-  volume: 0.05,
+  volume: soundEffectVolume,
 });
 
 export const addEventToVolumeSlider = () => {
@@ -13,6 +15,7 @@ export const addEventToVolumeSlider = () => {
     volumeSlider.addEventListener('input', () => {
       const newVolume = parseFloat(volumeSlider.value);
       bgm.volume(newVolume);
+      soundEffectVolume = newVolume;
     });
   }
 };
@@ -26,12 +29,13 @@ export const toggleMusic = (isSoundOn: boolean) => {
 };
 
 export const playSoundEffect = (soundName: string, pauseBGM: boolean = false) => {
+  if (!bgm.playing()) return; //If BGM is off, everything should be off
   if (pauseBGM) {
     bgm.pause();
   }
   const soundEffect = new Howl({
     src: [`/sounds/${soundName}.mp3`],
-    volume: 0.05,
+    volume: soundEffectVolume,
   });
   soundEffect.play();
   if (pauseBGM) {
